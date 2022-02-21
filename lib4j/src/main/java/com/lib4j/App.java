4374d02595f898087428dbdf7a24dcbb9493b16f
zkfy <lib4net@163.com>
2022-02-21 14:34:52 +0800

    更新目录结构

---
+++ lib4j/src/main/java/com/lib4j/App.java
@@ -0,0 +1,22 @@
+package com.lib4j;
+
+
+import com.lib4j.security.Md5;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println(Md5.encrypt("abc"));
+        System.out.println("Hello World!");
+    }
+}
